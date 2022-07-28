45cf472d1d243a60cf385b509a535722edc08e75
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-07-28 15:28:22 +0200

    Add example of using binding from query and route

--- WebApiApp/Controllers/ProjectsController.cs
+++ WebApiApp/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApiApp.Models;
 
 namespace WebApiApp.Controllers
 {
@@ -17,6 +18,40 @@ namespace WebApiApp.Controllers
         public IActionResult GetById(int id)
         {
             return Ok($"Reading project: #{id}");
+        }
+
+        /// <summary>
+        /// api/projects/{pid}/tickets/{tid}
+        /// </summary>
+        /// <param name="projectId"></param>
+        /// <param name="ticketId"></param>
+        /// <returns></returns>
+        //[HttpGet]
+        //[Route("{projectId}/tickets")]
+        //public IActionResult GetProjectTicket(int projectId, [FromQuery] int ticketId)
+        //{
+        //    if (ticketId == 0)
+        //    {
+        //        return Ok($"Reading all the tickets belong to project #{projectId}");
+        //    }
+        //    return Ok($"Reading project: #{projectId} and ticket #{ticketId}");
+        //}
+
+
+        [HttpGet]
+        [Route("{projectid}/tickets")]
+        public IActionResult GetProjectTicket([FromQuery] Ticket ticket)
+        {
+            if (ticket == null)
+            {
+                return BadRequest("Parameters are not provided properly!");
+            }
+            if (ticket.TicketId == 0)
+            {
+                return Ok($"Reading all the tickets belong to project #{ticket.ProjectId}");
+            }
+            return Ok($"Reading project: #{ticket.ProjectId} and ticket #{ticket.TicketId}" +
+                $" ,title: {ticket.Title}, decription: {ticket.Description}");
         }
 
         [HttpPost]
---
+++ WebApiApp/Models/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiApp.Models
+{
+    public class Ticket
+    {
+        [FromQuery(Name ="ticketid")]
+        public int TicketId { get; set; }
+
+        [FromRoute(Name = "projectid")]
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
